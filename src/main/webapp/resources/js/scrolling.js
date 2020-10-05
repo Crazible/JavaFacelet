1179c3b1a13d01a39f788403a602f4cb5ec0a480
Bobur <bobur.xxi@gmail.com>
2020-10-06 01:51:52 +0300

    Added navigation between pages

--- src/main/webapp/resources/js/scrolling.js
+++ src/main/webapp/resources/js/scrolling.js
@@ -14,3 +14,15 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove('sticky');
     }
 })
+
+const clockUI = document.querySelector('.navbar-inner .ui-clock');
+const clockCircle = document.querySelector('#clock');
+
+// Setting listener for clock ui
+window.addEventListener('scroll', function () {
+    if (clockCircle.getBoundingClientRect().bottom < 0) {
+        clockUI.classList.remove('invisible');
+    } else {
+        clockUI.classList.add('invisible');
+    }
+})
