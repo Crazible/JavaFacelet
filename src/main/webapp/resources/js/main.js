0574b6605f4d9e7cf61e57dfef049740ef660529
Bobur <bobur.xxi@gmail.com>
2020-10-25 17:00:07 +0300

    Added form items

---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,24 @@
+
+
+
+const selected = "selected",
+      graph_hidden = "graph-hidden",
+      table_hidden = "table-hidden";
+
+const menuList = document.querySelectorAll('div.menu'),
+      graph = document.querySelector('div.graph-field'),
+      table = document.querySelector('#table');
+
+const onGraph = function() {
+    menuList[0].classList.add(selected);
+    menuList[1].classList.remove(selected);
+    graph.classList.remove(graph_hidden);
+    table.classList.add(table_hidden);
+}
+
+const onTable = function() {
+    menuList[0].classList.remove(selected);
+    menuList[1].classList.add(selected);
+    graph.classList.add(graph_hidden);
+    table.classList.remove(table_hidden);
+}
